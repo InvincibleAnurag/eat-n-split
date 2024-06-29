a92ac85df2a434fe79510080bf90c2c9d7d994e3
Anurag Pandey <invincibleanurag5@gmail.com>
2024-06-29 11:11:11 +0530

    Created the friend list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
 
-function App() {
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+       <div className="sidebar">
+         <FriendList/>
+         {/* <button className="button">Add Friend</button> */}
+         <AddFriend/>
+       </div>
+       <SplitBill/>
     </div>
-  );
+  )
 }
 
-export default App;
+function FriendList() {
+  const friendList = initialFriends;
+  return <ul> 
+     {
+     friendList.map((friend)=> (<Friend friend={friend} key={friend.id}/>))
+     }
+  </ul>
+}
+
+function Friend({friend}) {
+     return <li>
+          <img src={friend.image}/>
+          <h3>{friend.name}</h3>
+          <button className="button">Select</button>
+     </li>
+    
+}
+
+function AddFriend() {
+   return  <div>
+      <form>
+             <div>
+              Friend name
+              <input type="text"/>
+              </div><br/>
+             <div>
+              Image URL
+              <input type="text"/>
+              </div><br/>
+             <button className="button">Add</button>
+            
+       </form>
+       <button className="button">Add</button>
+   </div>
+   
+}
+
+function SplitBill() {
+   return <form>
+      <h2>SPLIT A BILL WITH ANTHONY</h2>
+      <br/>
+          <div>
+          <label>Bill value</label>   
+          <input type="text"/>
+          </div>
+          <br/>
+          <div>Bill value  <input type="text"/>
+          </div>
+          <br/>
+          <div>Bill value  <input type="text"/>
+          </div>
+          <br/>
+          <div>Bill value  <input type="text"/>
+          </div>
+          <br/>
+          <button className="button">Splitbill</button>
+      </form>
+}
